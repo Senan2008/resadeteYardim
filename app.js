4effd0f1ab26dce36770fa38708eb622189df80e
ToxicG <74834749+Senan2008@users.noreply.github.com>
2025-05-10 20:59:04 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function degistir(){
+    document.getElementById("main").style.display = "none";
+    document.getElementById("gizli").style.display= "block";
+}
+
+function geri(){
+    document.getElementById("gizli").style.display = "none";
+    document.getElementById("main").style.display = "block";
+}
