78c0ba42ffb54c5f83d05a5c8559cdbb210fb189
ToxicG <74834749+Senan2008@users.noreply.github.com>
2025-05-25 13:16:25 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
-function degistir(){
+function degistir() {
     document.getElementById("main").style.display = "none";
-    document.getElementById("gizli").style.display= "block";
+    document.getElementById("gizli").style.display = "block";
 }
 
-function geri(){
+function geri() {
     document.getElementById("gizli").style.display = "none";
     document.getElementById("main").style.display = "block";
 }
+
+function showLoader() {
+    document.getElementById('container').style.display = 'flex'; // container'ı göster
+    document.getElementById('loader').style.display = 'block';   // loader'ı göster
+}
+
+function hideLoader() {
+    document.getElementById('container').style.display = 'none'; // container'ı gizle
+    document.getElementById('loader').style.display = 'none';    // loader'ı gizle
+}
+
+const button = document.getElementById('bagisEtBTN');
+const container = document.getElementById('container');
+
+button.addEventListener("click", () => {
+    showLoader();
+
+    setTimeout(() => {
+        hideLoader();
+        degistir();
+        button.style.display = 'none';
+    }, 600);
+});
+
+// Geri butonunu aktif et
+const geriBtn = document.querySelector('.geriBtn');
+geriBtn.addEventListener("click", () => {
+    showLoader();
+    setTimeout(() => {
+        hideLoader();
+        geri();
+        button.style.display = 'block'; // Butonu geri getir
+    }, 600);
+});
